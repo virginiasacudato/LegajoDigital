63f774be2fc18af782e6c54ba1009e85a1f82755
virginiasacudato <virginiasacudato@gmail.com>
2022-09-21 17:24:48 -0300

    Ref

--- test/Scripts/test_a_cert_firma.py
+++ test/Scripts/test_a_cert_firma.py
@@ -2,22 +2,21 @@ from src.TestBase.WebDriverSetup import WebDriverSetup
 from src.PageObject.Pages.CertificadoFima import CertificadoFirma
 
 
+class Test_CertFirma(WebDriverSetup):
 
-#class Test_CertFirma(WebDriverSetup):
+    def test_a_alta(self):
+        driver = self.driver
+        cert_firma = CertificadoFirma(driver)
+        cert_firma.alt_cert_raiz()
 
-    #def test_a_alta(self):
-    #    driver = self.driver
-    #    cert_firma = CertificadoFirma(driver)
-    #    cert_firma.alt_cert_raiz()
+    def test_b_baja(self):
+        driver = self.driver
+        cert_firma = CertificadoFirma(driver)
+        cert_firma.baja_cert_raiz()
 
-    #def test_b_baja(self):
-    #    driver = self.driver
-    #    cert_firma = CertificadoFirma(driver)
-    #    cert_firma.baja_cert_raiz()
-
-    #def test_c_mod(self):
-    #    driver = self.driver
-    #    cert_firma = CertificadoFirma(driver)
-    #    cert_firma.alt_cert_raiz()
+    def test_c_mod(self):
+        driver = self.driver
+        cert_firma = CertificadoFirma(driver)
+        cert_firma.alt_cert_raiz()
 
 
