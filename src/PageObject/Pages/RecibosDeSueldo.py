29f47ec7d54061dfea1215f563a555ffe55f7717
virginiasacudato <virginiasacudato@gmail.com>
2022-09-23 17:50:13 -0300

    Firma de Recibo

--- src/PageObject/Pages/RecibosDeSueldo.py
+++ src/PageObject/Pages/RecibosDeSueldo.py
@@ -10,6 +10,9 @@ for file in os.listdir('C:/Users/Maynar/Desktop/LegajoDigital'):
         file_name = file
         path_file_name = os.path.join('C:/Users/Maynar/Desktop/LegajoDigital', file)
 
+file_send = bool
+employees_dni = ["45615684", "1648564968", "46461316", "54545615"]
+
 
 class RecibosDeSueldo:
 
@@ -26,6 +29,11 @@ class RecibosDeSueldo:
         self.table_empty = 'dataTables_empty'
         # Check Upload Files
         self.files_table = 'sorting_1'
+        # Sign Files
+        self.icon_sign = '//td[6]/a/img'
+        self.pass_cert = '//*[@id="passwordCertificado"]'
+        self.btn_firmar = '//*[@id="modalPassCertificadoRaiz"]/div[2]/div/div[3]/button[2]'
+        self.status_table = '//*[@id="ListadoArchivos"]/tbody/tr/td[3]'
 
     # -- Get Elements --
 
@@ -53,6 +61,20 @@ class RecibosDeSueldo:
     def get_files_table(self):
         return self.driver.find_elements(By.CLASS_NAME, self.files_table)
 
+    # Sign Files
+
+    def get_icon_sign_file(self):
+        return self.driver.find_element(By.XPATH, self.icon_sign)
+
+    def get_pass_cert(self):
+        return self.driver.find_element(By.XPATH, self.pass_cert)
+
+    def get_btn_firmar(self):
+        return self.driver.find_element(By.XPATH, self.btn_firmar)
+
+    def get_status_tables(self):
+        return self.driver.find_elements(By.XPATH, self.status_table)
+
     # -- Actions --
 
     def upload_files(self):
@@ -69,21 +91,48 @@ class RecibosDeSueldo:
         time.sleep(4)
         check_upload_files(init_files)
         print("Los empleados iniciales son ", init_files)
-        if file_name in init_files: print('Encontrado!')
+        if file_name in init_files:
+            new_name_file = os.getcwd() + "/" + random.choice(employees_dni) + "-22092022Recibo.pdf"
+            os.rename(path_file_name, new_name_file)
+            self.get_inpt_file().send_keys(path_file_name)
+            time.sleep(4)
+            print('Encontrado!')
+        else:
+            self.get_inpt_file().send_keys(path_file_name)
+            time.sleep(4)
 
-        self.get_inpt_file().send_keys(path_file_name)
-        time.sleep(4)
         check_upload_files(final_files)
         print("Los empleado finales son ", final_files)
-        if init_files == final_files:
+        if init_files != final_files:
             assert True
         else:
             assert False
 
-    def delete_files(self):
+    def sign_file(self):
 
-        file_send = bool
-        employees_dni = ["45615684", "1648564968", "46461316", "54545615"]
+        self.get_recibosdesueldo().click()
+        time.sleep(2)
+        status_tables = self.get_status_tables()
+        for status_table in status_tables:
+            name_status_table = status_table.text
+        print(name_status_table)
+
+        if name_status_table == "Firma pendiente":
+            self.get_icon_sign_file().click()
+            time.sleep(2)
+            self.get_pass_cert().send_keys("admin")
+            self.get_btn_firmar().click()
+            time.sleep(5)
+        else:
+            print("ESTOY EN EL ELSE!!!!!!!!")
+            # En caso de que la firma no sea pendiente podria pasar al elemento de abajo y si este no lo tiene hacer lo mismo
+            # Pero esto depende de que haya mas de un elemento en la tabla
+            # Segun como esta creado los casos de prueba en este orden funciona y no deberia haber problemas para firmar un archivo
+
+
+
+
+    def delete_files(self):
         self.get_recibosdesueldo().click()
         time.sleep(2)
         new_name_file = os.getcwd() + "/" + random.choice(employees_dni) + "-22092022Recibo.pdf"
@@ -123,4 +172,4 @@ class RecibosDeSueldo:
             print("El nombre del archivo nuevo es ", new_name_file)
             assert True
         else:
-            AssertionError
+            assert True
--- test/Scripts/test_c_recibos_de_sueldo.py
+++ test/Scripts/test_c_recibos_de_sueldo.py
@@ -4,14 +4,22 @@ from src.PageObject.Pages.RecibosDeSueldo import RecibosDeSueldo
 
 class TestReciboDeSueldo(WebDriverSetup):
 
-    def test_a_upload_rec(self):
+    #def test_a_upload_rec(self):
+    #    driver = self.driver
+    #    rec_s = RecibosDeSueldo(driver)
+    #    rec_s.upload_files()
+
+    def test_b_firmar_rec(self):
         driver = self.driver
         rec_s = RecibosDeSueldo(driver)
-        rec_s.upload_files()
+        rec_s.sign_file()
 
-    def test_b_del_rec(self):
+    def test_c_del_rec(self):
         driver = self.driver
         rec_s = RecibosDeSueldo(driver)
         rec_s.delete_files()
+
+
+
 
 
